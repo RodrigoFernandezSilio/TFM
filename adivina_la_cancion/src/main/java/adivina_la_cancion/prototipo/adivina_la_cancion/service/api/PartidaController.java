7716b15b991451c346693374fbf58477d1ceeffd
Rodrigo Fernandez Silio <rodrigo.fernandezs@alumnos.unican.es>
2024-11-21 15:47:03 +0100

    Nueva característica: Permitir solicitudes desde http://localhost:4200

--- adivina_la_cancion/src/main/java/adivina_la_cancion/prototipo/adivina_la_cancion/service/api/PartidaController.java
+++ adivina_la_cancion/src/main/java/adivina_la_cancion/prototipo/adivina_la_cancion/service/api/PartidaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +25,7 @@ import adivina_la_cancion.prototipo.adivina_la_cancion.service.PartidaService;
 
 @RestController
 @RequestMapping("/partidas")
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600) // Permitir solicitudes desde http://localhost:4200
 public class PartidaController {
 
     @Autowired
--- adivina_la_cancion/src/main/java/adivina_la_cancion/prototipo/adivina_la_cancion/service/api/PlaylistController.java
+++ adivina_la_cancion/src/main/java/adivina_la_cancion/prototipo/adivina_la_cancion/service/api/PlaylistController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import adivina_la_cancion.prototipo.adivina_la_cancion.repositories.PlaylistRepo
 
 @RestController
 @RequestMapping("/playlists")
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600) // Permitir solicitudes desde http://localhost:4200
 public class PlaylistController {
 
     @Autowired
