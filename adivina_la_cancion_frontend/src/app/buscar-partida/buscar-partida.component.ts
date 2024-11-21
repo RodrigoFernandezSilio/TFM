cfa8c6c1c967c83b22975d5410088a5c8b9c4a75
Rodrigo Fernandez Silio <rodrigo.fernandezs@alumnos.unican.es>
2024-11-21 16:34:35 +0100

    Nueva característica: Esqueleto de buscar partida y crear partida

---
+++ adivina_la_cancion_frontend/src/app/buscar-partida/buscar-partida.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buscar-partida',
+  standalone: true,
+  imports: [],
+  templateUrl: './buscar-partida.component.html',
+  styleUrl: './buscar-partida.component.css'
+})
+export class BuscarPartidaComponent {
+
+}
