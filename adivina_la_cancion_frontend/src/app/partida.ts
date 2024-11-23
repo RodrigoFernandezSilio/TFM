5ca068cd45843ac7baebca1ceaefb14f53f80ec6
Rodrigo Fernandez Silio <rodrigo.fernandezs@alumnos.unican.es>
2024-11-23 09:33:19 +0100

    Nueva característica: Crear partida y usuario

---
+++ adivina_la_cancion_frontend/src/app/partida.ts
@@ -0,0 +1,46 @@
+import { Usuario } from "./usuario";
+
+export interface Partida {
+    id: number;
+    estado: EstadoPartida;
+    usuarios: Usuario[];
+    numMaxRondas: number;
+    rondas: Ronda[];
+    playlistID: Playlist;
+    privada: boolean;
+    codigoAcceso: string;
+}
+
+enum EstadoPartida {
+	NO_INICIADA,
+    INICIADA,
+    FINALIZADA
+}
+
+interface Ronda {
+    id: number;
+    canciones: Cancion[];
+    cancionCorrecta: Cancion;
+}
+
+interface Playlist {
+    id: number;
+    nombre: string;
+}
+
+interface Cancion {
+    id: number;
+    nombre: string;
+    audioURL: string;
+}
+
+
+
+export interface PartidaDTO {
+    numMaxUsuarios: number;
+    usuarioID: number;
+    numMaxRondas: number;
+    playlistID: number;
+    privada: boolean;
+    codigoAcceso: string;
+}
