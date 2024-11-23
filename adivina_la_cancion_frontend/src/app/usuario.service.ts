0265c2384a00b12dc0f167c7996228b586398497
Rodrigo Fernandez Silio <rodrigo.fernandezs@alumnos.unican.es>
2024-11-23 11:10:11 +0100

    Refactorizar: Modificar crearUsuario para que devuelva el usuarioID

--- adivina_la_cancion_frontend/src/app/usuario.service.ts
+++ adivina_la_cancion_frontend/src/app/usuario.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, tap } from 'rxjs';
+import { Usuario } from './usuario';
 
 @Injectable({
   providedIn: 'root'
@@ -9,17 +10,23 @@ export class UsuarioService {
 
   usuariosURL = "http://localhost:8080/usuarios"
 
+  usuario: Usuario | null = null;
+
   constructor(private http: HttpClient) { }
 
-  crearUsuario(nombreUsuario: string): Observable<any> {
+  crearUsuario(nombreUsuario: string): Observable<number> {
     console.log("Creando usuario...")
 
     const url = `${this.usuariosURL}/${nombreUsuario}`;
 
-    return this.http.post<any>(url, null).pipe(
-      tap(_ => {
-        console.log(`Usuario ${nombreUsuario} creado`);
+    return this.http.post<number>(url, null).pipe(
+      tap((usuarioID) => {
+        this.usuario = {
+          id: usuarioID,
+          nombre: nombreUsuario,
+        };
+        console.log(`Usuario creado`);
       })
-    )
+    );
   }
 }
