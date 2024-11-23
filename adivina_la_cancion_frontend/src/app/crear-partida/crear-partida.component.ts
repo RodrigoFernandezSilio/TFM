a879022e2f6cc1d1d8bfd49c25a3c6bbcf24a5f3
Rodrigo Fernandez Silio <rodrigo.fernandezs@alumnos.unican.es>
2024-11-23 11:11:08 +0100

    Nueva característica: Crear componente crear-partida

--- adivina_la_cancion_frontend/src/app/crear-partida/crear-partida.component.ts
+++ adivina_la_cancion_frontend/src/app/crear-partida/crear-partida.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { Partida, PartidaDTO } from '../partida';
+import { PartidaService } from '../partida.service';
+import { FormsModule } from '@angular/forms';
+import { UsuarioService } from '../usuario.service';
 
 @Component({
   selector: 'app-crear-partida',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './crear-partida.component.html',
   styleUrl: './crear-partida.component.css'
 })
 export class CrearPartidaComponent {
+
+  partidaDTO: PartidaDTO = {
+    numMaxUsuarios: 0,
+    usuarioID: 0,
+    numMaxRondas: 0,
+    playlistID: 0,
+    privada: false,
+    codigoAcceso: '',
+  };
+
+  constructor(private partidaService: PartidaService, private usuarioService: UsuarioService) { }
+
+  ngOnInit() {
+    this.partidaDTO.usuarioID = this.usuarioService.usuario!.id;
+  }
+
+  crearPartida() {
+    this.partidaService.crearPartida(this.partidaDTO).subscribe(partidaID => {
+      this.partidaService.anhadirUsuario(partidaID, this.usuarioService.usuario!.id);
+    });
+  }
 
 }
